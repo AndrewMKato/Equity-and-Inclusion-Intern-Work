d8beb0d4e1643b6d5494cc0d1d2e26719777dbaf
Andrew Kato <andrewmkato@gmail.com>
2022-07-19 18:58:22 -0700

    Simple generation with one file. OOP needed.

---
+++ UC Riverside Word Cloud/main.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+
+with open('Inclusive_Classrooms_for_web.txt', 'r', encoding='utf-8') as file:
+    classrooms_text: str = file.read()
+
+stop_words = list(STOPWORDS)
+stop_words.append('UCR')
+stop_words.append('Riverside')
+print(stop_words)
+
+word_cloud = WordCloud(
+    background_color='white',
+    stopwords=stop_words,
+    height=500, # In pixels.
+    width=500 # In pixels.
+)
+
+word_cloud.generate(classrooms_text)
+
+word_cloud.to_file('inclusive_classrooms_URI.png')
