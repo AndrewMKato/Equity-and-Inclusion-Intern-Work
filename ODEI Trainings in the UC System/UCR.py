b697bbe235841a4ed9610032c4effb9cce40c53f
Andrew Kato <andrewmkato@gmail.com>
2022-07-21 01:41:23 -0700

    Beginning storing data on trainings options from each UC

---
+++ ODEI Trainings in the UC System/UCR.py
@@ -0,0 +1,24 @@
+# Trainings Offered by the UCR ODEI for campus organizations/groups. 
+ucr_trainings = [
+    'Creating and Sustaining an Inclusive Departmental Cultural Climate',
+    'Improving Diversity, Equity & Inclusion in Search Hiring Processes',
+    'Inclusive Excellence & Unconscious/Implicit Bias: Understanding the Principles & Vocabulary',
+    'Diversity, Equity & Inclusion on the American Campus for Student Financial Aid Administrators',
+    'Inclusive Working Climates for Facilities Employees',
+    'Diversity, Equity & Inclusion in Academic Professionalization for Graduate Students',
+    'How to Write a Diversity Statement',
+    'Diversity, Equity & Inclusion Initiatives at UC Riverside',
+    'UC Systemwide Diversity, Equity & Inclusion Initiatives at UC Riverside',
+    'Inclusive Cultural Competence',
+    'Decolonizing Leadership Models in Higher Education',
+    'Diversity, Equity & Inclusion Work in Residential Life',
+    'Diversity, Equity & Inclusion for Student Staff Employees',
+    'Diversity, Equity & Inclusion in Classroom Instruction/Pedgogy and Course/Syllabus Design',
+    'Institutional Racism and Improving Climate',
+    'Discrimination and COVID-19',
+    'Supervisor Performance Management',
+    'Inclusive Climates for International Students and Scholars',
+    "Forms of Privilege and \"White Fragility\"",
+    'Individual and Collective Accountability',
+    'Social Movements'
+]
