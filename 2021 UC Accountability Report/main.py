c63aa78a317ebd0a20a3638ba89dc8fc15417939
Andrew Kato <andrewmkato@gmail.com>
2022-07-21 23:15:05 -0700

    Trying things out with np/pd

--- 2021 UC Accountability Report/main.py
+++ 2021 UC Accountability Report/main.py
@@ -1,9 +1,21 @@
-from msilib.schema import File
+import csv
 import matplotlib.pyplot as plt
 import pandas as pd, numpy as np
-import csv
+import sklearn.linear_model
 
-with open('chapter06data2021.csv', 'r') as file:
-    rd = csv.reader(file)
-    for row in rd:
-        print(row)
+# with open('chapter06data2021.csv', 'r') as file:
+#     rd = csv.reader(file)
+#     for row in rd:
+#         print(row)
+
+df: pd.DataFrame = pd.read_csv('chapter06data2021.csv')
+
+X = np.c_[df['_']]
+y = np.c_[df['_']]
+
+df.plot(kind='scatter', x='_', y='_')
+plt.show()
+
+model = sklearn.linear_model.LinearRegression()
+
+model.fit(X, y)
